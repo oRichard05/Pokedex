afbd270fa81792d73b1e4378414fafa1abd1e4ad
playerking3 <murilomarquesdecastro@gmail.com>
2023-04-14 16:08:24 -0300

    anexado javascript com html

--- script/script.js
+++ script/script.js
@@ -1,9 +1,9 @@
-var form = document.querySelector("#formularo")
+var form = document.querySelector("#form-pesquisa")
 
 form.addEventListener("submit", function(event){
     event.preventDefault()
 
-    var nome = document.querySelector("input[name=pesquisaForm]").value
+    var nome = document.querySelector("input[name=pesquisa]").value
     var url = "https://pokeapi.co/api/v2/pokemon/"+ nome
 
     console.log (url)
@@ -16,17 +16,20 @@ form.addEventListener("submit", function(event){
         .then(function(data){
             console.log(data)
 
+            var peso = document.querySelector("#peso")
+            var altura = document.querySelector("#altura")
             var conteudo = document.querySelector("#conteudo")
+            var imagem = document.querySelector("#pokemon-img")
 
-            conteudo.innerHTML = "<h3>" + data.name + "</h3>"
-            conteudo.innerHTML +="<img src='"+data.sprites.front_default+"' alt='' id='pk-img'>"
+            conteudo.innerHTML = "<p>" + data.name + "</p>"
+            imagem.innerHTML ="<img src='"+data.sprites.front_default+"' alt='' id='pk-img'>"
             data.types.forEach((tipos) => {
                 var nome = tipos.type.name
                 conteudo.innerHTML +="<p> Tipo: " + nome + "</p>"
             })
             conteudo.innerHTML += "<p> ID: "+ data.id +"</p>"
-            conteudo.innerHTML += "<p> Tamanho: "+(data.height / 10)+"m </p>"  
-            conteudo.innerHTML += "<p> Peso: "+(data.weight / 10)+"Kg </p>"
+            altura.innerHTML = "<p>"+(data.height / 10)+"m </p>"  
+            peso.innerHTML = "<p>"+(data.weight / 10)+"Kg </p>"
 
             if (data.id <= 151){
                 conteudo.innerHTML += "<p>1º Geração</p>"
